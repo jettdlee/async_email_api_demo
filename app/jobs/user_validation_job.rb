7aa707e87662585863b11a4d5ee1f91682c2e0d7
Jet Lee <jettdlee@gmail.com>
2025-06-09 19:43:26 +0100

    fix spec and clean up

--- app/jobs/user_validation_job.rb
+++ app/jobs/user_validation_job.rb
@@ -5,7 +5,7 @@ class UserValidationJob < ApplicationJob
     upload = Upload.find(upload_id)
     return unless data
     user = User.new(name: data['name'], email: data['email'])
-    unless user.validate
+    unless user.validate.value
       error_details = {
         name: user.name,
         email: user.email,
@@ -18,16 +18,8 @@ class UserValidationJob < ApplicationJob
       Rails.logger.info("Upload #{upload.id} - Validation succeeded for #{user.email}")
     end
     REDIS.hincrby("#{upload.redis_key}:progress", "processedRecords", 1)
-    upload.save!
   rescue => e
     Rails.logger.error("Upload #{upload_id} - Unexpected error: #{e.class} - #{e.message}")
     raise
-  end
-
-  private
-
-  def progress_cache_json
-    return @_progress_cache if defined?(@_progress_cache)
-    @_progress_cache = JSON.parse($redis.get(redis_key))
   end
 end
